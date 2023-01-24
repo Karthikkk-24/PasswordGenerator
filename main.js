1372101c3e8daabc75d6c63764160aa370caee2e
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-01-24 08:59:45 +0530

    Design Ready

---
+++ main.js
@@ -0,0 +1,11 @@
+// let add = document.getElementById('add_btn');
+let password = document.getElementById('password');
+
+// add.document.addEventListener('click', function () {
+//     // password.value = "karthik";
+// });
+
+function generate_password() {
+    password.value = "karthik";
+    // alert("something");
+}
